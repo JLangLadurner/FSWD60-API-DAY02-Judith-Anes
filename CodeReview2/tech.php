fb2d9b9e8b07691e2608313e80237a10ae4147ed
Anesto27 <anes.2503@gmx.at>
2019-03-25 09:29:35 +0100

    codereview 2 insert into folder

---
+++ CodeReview2/tech.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>CodeReview 2 </title>
+	<link rel="stylesheet" href="CodeReview.css">
+</head>
+<body>
+	<div class="container">
+		<div id="header">
+			<header>
+				<div id="first">
+					<img src="logo-newspaper-cf.png" alt="logo">
+				</div>
+				<div id="second">
+				<a href= https://codefactory.wien/de/home/><img src="banner-1.png" alt="codefactory banner1"></a>
+			</div>
+			</header>
+		</div>
+		<div id="navBar">
+			<ul>
+				<li><a href="CodeReview.html">Home</a></li> 
+				<li><a href="tech.php">Tech</a></li> 
+				<li><a href="">Culture</a></li> 
+				<li><a href="">Video</a></li> 
+			</ul>
+		</div>
+		<main class="main">
+		<div id="mainTitle1">
+			
+		</div>
+		<div id="mainTitle2">
+			<p>Tech</p>
+		</div>
+		
+		</main>
+		<footer class="footer">
+			<div id="navBar2">
+				<ul>
+				<li><a href="#">Home</a></li> 
+				<li><a href="#">Tech</a></li> 
+				<li><a href="#">Culture</a></li> 
+				<li><a href="#">Video</a></li> 
+			</ul>
+			</div>
+			<div id="banner5">
+				<a href= https://codefactory.wien/de/home/"><img src="logo-newspaper-cf.png" alt="Banner5"></a>
+			</div>
+				<h5>Smajic Anes - CodeFactory 2018</h5>
+
+		</footer>
+	</div>
+		
+</body>
+</html>
