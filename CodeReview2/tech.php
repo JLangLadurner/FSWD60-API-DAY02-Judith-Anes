342251093a468318d51d9b23133645dfabc12f4f
Anesto27 <anes.2503@gmx.at>
2019-03-25 09:40:55 +0100

    add right path for the pics

--- CodeReview2/tech.php
+++ CodeReview2/tech.php
@@ -3,17 +3,17 @@
 <head>
 	<meta charset="UTF-8">
 	<title>CodeReview 2 </title>
-	<link rel="stylesheet" href="CodeReview.css">
+	<link rel="stylesheet" href="css/CodeReview.css">
 </head>
 <body>
 	<div class="container">
 		<div id="header">
 			<header>
 				<div id="first">
-					<img src="logo-newspaper-cf.png" alt="logo">
+					<img src="img/logo-newspaper-cf.png" alt="logo">
 				</div>
 				<div id="second">
-				<a href= https://codefactory.wien/de/home/><img src="banner-1.png" alt="codefactory banner1"></a>
+				<a href= "https://codefactory.wien/de/home/"><img src="img/banner-1.png" alt="codefactory banner1"></a>
 			</div>
 			</header>
 		</div>
@@ -37,14 +37,14 @@
 		<footer class="footer">
 			<div id="navBar2">
 				<ul>
-				<li><a href="#">Home</a></li> 
-				<li><a href="#">Tech</a></li> 
+				<li><a href="CodeReview.html">Home</a></li> 
+				<li><a href="tech.php">Tech</a></li> 
 				<li><a href="#">Culture</a></li> 
 				<li><a href="#">Video</a></li> 
 			</ul>
 			</div>
 			<div id="banner5">
-				<a href= https://codefactory.wien/de/home/"><img src="logo-newspaper-cf.png" alt="Banner5"></a>
+				<a href= "https://codefactory.wien/de/home/"><img src="img/logo-newspaper-cf.png" alt="Banner5"></a>
 			</div>
 				<h5>Smajic Anes - CodeFactory 2018</h5>
 
